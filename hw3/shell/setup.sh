f10aed091c1839a0bb0abd129ac88a3395b4f424
Neverland1973556 <2556692804@qq.com>
2024-03-26 15:48:33 -0400

    hw3 start

---
+++ hw3/shell/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /cmshome/xuzhitao/cscd43
+rm -rf postgresql-7.4.13
+rm -f postgresql-7.4.13.tar.gz
+wget http://ftp-archives.postgresql.org/pub/source/v7.4.13/postgresql-7.4.13.tar.gz
+tar -zxvf postgresql-7.4.13.tar.gz
+cd postgresql-7.4.13
+
+# Install PostgreSQL. 
+./configure CFLAGS='-O0 -pipe -fcommon' --prefix=/cmshome/xuzhitao/cscd43/postgresql-7.4.13/ --enable-depend --without-readline
+make
+make install
+
+# Initialize a directory where PostgreSQL stores its data. ~
+mkdir data
+./bin/initdb -D ./data
